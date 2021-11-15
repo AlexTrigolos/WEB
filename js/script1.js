571003259ceb16edb477f21e6a2bdac54823ef2e
AlexTrigolos <67746495+AlexTrigolos@users.noreply.github.com>
2021-11-15 11:56:04 +0300

    v1.0 lab5

--- js/script1.js
+++ js/script1.js
@@ -1,31 +1,66 @@
-document.addEventListener('contextmenu', function (e) {
-    m = document.getElementById("drop_hidden").style;
-    if (m.display === 'block') {
-        m.display = 'none';
-    } else {
-        m.display = 'block';
-        m.position = 'absolute';
-    }
-}, false);
-// function ocm(){
-//     document.addEventListener("contextmenu", function (e){
-//         m = document.getElementById("drop_hidden").style;
-//         if (m.display === 'block') {
-//             m.display = 'none';
-//         } else {
-//             m.display = 'block';
-//             m.position = 'absolute';
-//         }
-//     }, false)
-// }
-var start;
-(function () {
-    start = window.performance.timing.navigationStart;
-})();
-
-function fin() {
-    let f = document.getElementById("footer")
-    let newfoot = f.innerHTML + ". Время вхождения: " + ((window.performance.timing.domContentLoadedEventEnd - start) / 1000) + " секунды.";
-    f.remove();
-    document.body.append(newfoot);
+function AddRow(obj) {
+    let table = document.querySelector("table");
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    // td.innerHTML = id.toString();
+    // tr.appendChild(td);
+    // td = document.createElement('td');
+    td.innerHTML = obj.day;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = obj.eo;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = obj.sub;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = obj.time;
+    tr.appendChild(td);
+    td = document.createElement('td');
+    td.innerHTML = obj.type;
+    tr.appendChild(td);
+    table.appendChild(tr);
 }
+// window.localStorage.clear()
+let id = 0
+window.addEventListener('load', function (e) {
+    let locstor = window.localStorage;
+    let i = 0;
+    let next;
+    while ((next = locstor.getItem(i.toString())) != null) {
+        let mblast = JSON.parse(next);
+        AddRow(mblast);
+        i++;
+        id++;
+    }
+    document.querySelector('#send').onclick = function (event) {
+        event.preventDefault();
+        let day = document.querySelector("#day").value;
+        let eo = document.querySelector("#eo").value;
+        let sub = document.querySelector("#sub").value;
+        let time = document.querySelector("#time").value;
+        let type = document.querySelector("#type").value;
+        let locstor = window.localStorage;
+        locstor.setItem(id.toString(), JSON.stringify({day: day, eo: eo, sub: sub, time: time, type: type}));
+        AddRow({day: day, eo: eo, sub: sub, time: time, type: type});
+        id++;
+    }
+    document.querySelector('#delall').onclick = function (event) {
+        event.preventDefault();
+        let table = document.querySelector("table");
+        for (let j = 0; j < id; j++){
+            table.deleteRow(id - j);
+        }
+        window.localStorage.clear();
+        id = 0;
+    }
+    document.querySelector('#delast').onclick = function (event) {
+        event.preventDefault();
+        let table = document.querySelector("table");
+        if(window.localStorage.length > 0) {
+            table.deleteRow(id);
+            window.localStorage.removeItem((id - 1).toString());
+            id--;
+        }
+    }
+}, false)
